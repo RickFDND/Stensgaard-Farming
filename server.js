d652effb19e351f7cf126ab3266681e712ac4339
RickFDND <rick.pijnacker@hva.nl>
2025-07-19 12:14:02 +0200

    server opzetten done

--- server.js
+++ server.js
@@ -1,16 +1,32 @@
 import express from 'express'
 
+// Importeer de Liquid package (ook als dependency via npm geïnstalleerd)
+import { Liquid } from 'liquidjs';
+
 // Maak een nieuwe Express applicatie aan, waarin we de server configureren
 const app = express()
 
 app.use(express.static('public'))
 
+// Stel Liquid in als 'view engine'
+const engine = new Liquid();
+app.engine('liquid', engine.express()); 
 
-
-
+app.set('views', './views')
 
 // Zorg dat werken met request data makkelijker wordt
 app.use(express.urlencoded({extended: true}))
+
+app.get('/', async function (request, response) {
+   // Render index.liquid uit de Views map en geef de opgehaalde data mee, in een variabele genaamd person
+   response.render('index.liquid')
+})
+
+app.post('/', async function (request, response) {
+  // Je zou hier data kunnen opslaan, of veranderen, of wat je maar wilt
+  // Er is nog geen afhandeling van een POST, dus stuur de bezoeker terug naar /
+  response.redirect(303, '/')
+})
 
 // Stel het poortnummer in waar Express op moet gaan luisteren
 // Lokaal is dit poort 8000, als dit ergens gehost wordt, is het waarschijnlijk poort 80
