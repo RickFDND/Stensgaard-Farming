6a7e11fa5e39c0d86020fc17ce3a094441b25137
RickFDND <rick.pijnacker@hva.nl>
2025-07-26 19:32:09 +0200

    domcontentloaded weggehaald #1

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const hamburgerButton = document.querySelector('#hamburger-button');
-  const navbar = document.querySelector('nav');
+const hamburgerButton = document.querySelector('#hamburger-button');
+const navbar = document.querySelector('nav');
 
   if (!hamburgerButton || !navbar) return;
 
-  hamburgerButton.addEventListener('click', function (e) {
-    e.preventDefault(); // voorkomt dat de <a href="#quick-links"> springt naar de footer
+hamburgerButton.addEventListener('click', function (e) {
+  e.preventDefault(); // voorkomt dat de <a href="#quick-links"> springt naar de footer
 
-    navbar.classList.toggle('nav-open'); // toggle menu zichtbaar
-    hamburgerButton.classList.toggle('active'); // toggle knop animatie
-  });
+  navbar.classList.toggle('nav-open'); // toggle menu zichtbaar
+  hamburgerButton.classList.toggle('active'); // toggle knop animatie
 });
